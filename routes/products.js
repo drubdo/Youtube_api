ea44fd34f006cde800f847915ba7c7147d9ceacc
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-04 17:46:54 -0500

    added joi package & validate function

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,22 @@
-const Product = require('../models/product');
-const express = require('express');
+const { Product, validate } = require('../models/product');
+const express = require("express");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-   
-        const product = new Product({
-            name: req.body.name,
-            description: req.body.description,
-            category: req.body.category,
-            price: req.body.price,
-            });
-            await product.save();
-            return res.send(product);
-            } catch (ex) {
-            return res.status(500).send(`Internal Server Error: ${ex}`);
-            }
-           });
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error);
+
+    const product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+    });
+    await product.save();
+    return res.send(product);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 module.exports = router;
