bec2a2677267260bf3924ed28ebfc6680ddaaf53
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-03 21:14:44 -0500

    added products.js/routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectDB = require("./startup/db");
 const express = require("express");
 const app = express();
+const products = require('./routes/products');
 
 connectDB();
 
 app.use(express.json());
+app.use('/api/products', products);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
