8f0bb60be80ced16c1fb90bd14af76c2336232f5
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-04 21:34:38 -0500

    added put in postman

--- routes/products.js
+++ routes/products.js
@@ -24,6 +24,43 @@ router.get('/', async (req, res) => {
     }
    });
 
+router.put('/:id', async (req, res) => {
+ try {
+ const { error } = validate(req.body);
+ if (error) return res.status(400).send(error);
+ const product = await Product.findByIdAndUpdate(
+ req.params.id,
+ {
+ name: req.body.name,
+ description: req.body.description,
+ category: req.body.category,
+ price: req.body.price,
+ },
+ { new: true }
+ );
+ if (!product)
+ return res.status(400).send(`The product with id "${req.params.id}" d
+oes not exist.`);
+ await product.save();
+ return res.send(product);
+ } catch (ex) {
+ return res.status(500).send(`Internal Server Error: ${ex}`);
+ }
+});    
+
+router.delete('/:id', async (req, res) => {
+    try {
+   
+    const product = await Product.findByIdAndRemove(req.params.id);
+    if (!product)
+    return res.status(400).send(`The product with id "${req.params.id}" d
+   oes not exist.`);
+    return res.send(product);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+   });
+
 router.post("/", async (req, res) => {
   try {
     const { error } = validate(req.body);
